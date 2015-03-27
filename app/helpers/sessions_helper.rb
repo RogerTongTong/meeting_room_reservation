400a16cdc78b20d830a48bcbf239bf35c9ae0b6b
Roger Tong <Roger.Tong@activenetwork.com>
2015-03-27 09:59:54 +0800

    Correct parameter of User.find_by

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
   end
 
   def current_user
-      @current_user ||= User.find_by(session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def log_in(user)
